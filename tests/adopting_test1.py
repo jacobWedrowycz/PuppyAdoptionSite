16b591f19d40a7d36fdd0ac2610366b224f21a4f
DESKTOP-ERP32IN\raqqu <raqqu117@o2.pl>
2023-05-28 22:53:41 +0200

    test1 additions

--- tests/adopting_test1.py
+++ tests/adopting_test1.py
@@ -4,6 +4,7 @@ import time
 
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Chrome(service=Service(r'C:\TestFiles\chromedriver.exe'))
 driver.get("https://spartantest-puppies.herokuapp.com/")
@@ -18,8 +19,32 @@ AdoptMeButton1 = driver.find_element(By.CSS_SELECTOR, "form.button_to[action='/a
                                                       "input.rounded_button")
 AdoptMeButton1.click()
 
+checkbox1 = driver.find_element(By.XPATH, "//input[@name='toy']")
+checkbox2 = driver.find_element(By.XPATH, "//input[@name='carrier']")
 
+if not checkbox1.is_selected():
+    checkbox1.click()
 
+if not checkbox2.is_selected():
+    checkbox2.click()
+
+ViewDetailsButton1 = driver.find_element(By.CSS_SELECTOR, "form.button_to[action='/orders/new'] input.rounded_button")
+ViewDetailsButton1.click()
+
+field_name = driver.find_element(By.ID, "order_name")
+field_address = driver.find_element(By.ID, "order_address")
+field_email = driver.find_element(By.ID, "order_email")
+field_pay_type = driver.find_element(By.ID, "order_pay_type")
+
+field_name.send_keys("Adam Salamon")
+field_address.send_keys("Stachiewicza 40a/18, 30-328 Kraków")
+field_email.send_keys("adam.salamon289@gmail.com")
+field_pay_type.click()
+option_check = driver.find_element(By.XPATH, "//select[@id='order_pay_type']/option[@value='Check']")
+option_check.click()
+
+PlaceOrderButton = driver.find_element(By.CSS_SELECTOR, "button.submit")
+PlaceOrderButton.click()
 
 time.sleep(5) #sleep for 5sec before closing browser
 
